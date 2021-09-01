74dd7360713212802d74e7b4dde615a951a0ebad
Roberto <beto.rodriguesds@gmail.com>
2021-09-01 09:23:31 -0300

    adjusts

--- app.ts
+++ app.ts
@@ -1,12 +1,9 @@
-import * as dotenv from "dotenv";
 import express from "express";
 import { PORT } from './app/config/config'
 import routes from './app/routes'
 import * as bodyParser from "body-parser";
-import { curriculumRouter } from "./app/routes/curriculumRouter";
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json());
 app.use(routes)
